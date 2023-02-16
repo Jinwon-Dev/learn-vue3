f4142919ecc3ab8c0c655e51051aab57191cd710
Jinwon-Dev <yjw9424@naver.com>
2023-02-16 23:07:29 +0900

    setting files

---
+++ .eslintrc.cjs
@@ -0,0 +1,26 @@
+/* eslint-env node */
+require("@rushstack/eslint-patch/modern-module-resolution");
+
+module.exports = {
+  root: true,
+  extends: [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@vue/eslint-config-prettier",
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  "rules": {
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "prettier/prettier": ["error", {
+      semi: true,
+      useTabs: true,
+      tabWidth: 2,
+      trailingComma: "all",
+      printwidth: 80,
+      bracketSpacing: true,
+      arrowParens: "avoid",
+    }],
+  },
+};
