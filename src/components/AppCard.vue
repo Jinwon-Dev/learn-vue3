fe181d338f7d33df83e39e461d03a73f5569275c
Jinwon-Dev <yjw9424@naver.com>
2023-03-09 19:51:50 +0900

    Props

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -1,32 +1,66 @@
 <template>
-	<div className="card">
-		<div className="card-body">
-			<h5 className="card-title red">Card title</h5>
-			<p className="card-text">
-				Some quick example text to build on the card title and make up the bulk
-				of the card's content.
-			</p>
-			<a href="#" className="btn btn-primary">Go somewhere</a>
+	<div class="card">
+		<div class="card-body">
+			<span class="badge bg-secondary">{{ typeName }}</span>
+			<h5 class="card-title red mt-2">{{ title }}</h5>
+			<p class="card-text">{{ contents }}</p>
+			<a href="#" class="btn" :class="isLikeClass" @click="toggleLike"
+				>좋아요</a
+			>
+			<br />
+			{{ obj }}
 		</div>
 	</div>
 </template>
 
 <script>
-import { ref } from "vue";
+import { computed } from "vue";
 
-console.log("AppCard Module");
+console.log("AppCard module");
 export default {
-	setup() {
-		console.log("AppCard setup()");
-		const color = ref("red");
-		color.value = "blue";
-		return { color };
+	props: {
+		type: {
+			type: String,
+			default: "news",
+			validator: value => {
+				return ["news", "notice"].includes(value);
+			},
+		},
+		title: {
+			type: String,
+			required: true,
+		},
+		contents: {
+			type: String,
+			required: true,
+		},
+		isLike: {
+			type: Boolean,
+			default: false,
+		},
+		obj: {
+			type: Object,
+			default: () => {},
+		},
+	},
+	emits: ["toggleLike"],
+	setup(props, context) {
+		console.log("props.title", props.title);
+		const isLikeClass = computed(() =>
+			props.isLike ? "btn-danger" : "btn-outline-danger",
+		);
+		const typeName = computed(() =>
+			props.type === "news" ? "뉴스" : "공지사항",
+		);
+		const toggleLike = () => {
+			// props.isLike = !props.isLike;
+			// props.obj.title = "김길동";
+			// context.emit("changeTitle");
+			context.emit("toggleLike");
+		};
+		return { isLikeClass, typeName, toggleLike };
 	},
 };
 </script>
 
-<style>
-.red {
-	color: v-bind(color) !important;
-}
-</style>
+<style></style>
