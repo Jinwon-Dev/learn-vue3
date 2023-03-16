7f4c30e4d191150fa1bbc1bfbc5f6aa6ee9064c8
Jinwon-Dev <yjw9424@naver.com>
2023-03-16 19:32:03 +0900

    Provide/Inject

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { createApp } from "vue";
 import App from "./App.vue";
-import "bootstrap/dist/js/bootstrap";
-import AppCard from "@/components/PostItem.vue";
+import "bootstrap/dist/js/bootstrap.js";
 
 const app = createApp(App);
 
-app.component("AppCard", AppCard);
+// app.component('AppCard', AppCard);
 
+app.provide("app-message", "app message 입니다");
+app.config.globalProperties.msg = "hello";
+app.provide("msg", "hello msg");
 app.mount("#app");
